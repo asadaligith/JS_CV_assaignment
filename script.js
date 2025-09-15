7cdbcf0346d323a7a221def284e0a96c11deebd5
asadaligith <asad2747982@gmail.com>
2025-09-15 12:05:57 +0500

    updated

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-is_shown = false;
+var is_shown = false;
 
 function showpic(){
      pic = document.getElementById("picture");
@@ -12,4 +12,27 @@ function showpic(){
         pic.innerHTML = '';
         is_shown = false;
      }     
+}
+
+
+
+
+function formsubmit(event){
+   let name = document.getElementById("name").value;
+   let email = document.getElementById("email").value;
+
+   event.preventDefault(); // Prevent form submission
+
+   if(name ==="" && email ===""){
+      alert("Please fill in both fields Name and Email.");
+   }
+   else if(name ===""){
+      alert("Please enter your Name.");
+   }
+   else if(email ===""){
+      alert("Please enter your Email.");
+   
+    } else {
+      alert("Thank you " + name + "! We have received your email: " + email);
+   }
 }
